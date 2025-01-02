e1e6f0200672eb4ab4c88b1dac4644e606ab3d5e
indhu96 <147133485+indhu96@users.noreply.github.com>
2025-01-02 10:44:17 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,7 @@
-import java.util.*;
+
 public class Helloworld{
    public static void main(String[] args);
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Hello World");
+    System.out.print("Hello World");
   }
 }
 
